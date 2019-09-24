0fe233aa8704ed658e2240d7e8fbef2a542d3723
Pablo Gumilla <pgumilla@eventbrite.com>
2019-09-24 16:56:15 -0300

    Add Header as a separate bundle

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import './scss/app.scss'
 import { html } from "../README.md";
-import Header from './components/Header'
+
+const Header = lazy(() => import('./components/Header'));
 
 class HelloMessage extends React.Component {
     render() {
         return <div>
-            <Header/>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Header/>
+            </Suspense>
             <div className="container">
                 <h1>Markdown content as HTML</h1>
                 <div className="content" dangerouslySetInnerHTML={{__html: html}}></div>
